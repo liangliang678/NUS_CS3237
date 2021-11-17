2351982cde3fd0f6fb3fa5e738bfa1566b39e015
吴俊亮 <wujunliang18@mails.ucas.ac.cn>
2021-11-17 20:31:04 +0800

    add support of multi tags

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # CS3237 Project
 
+## Setup of CC2650 SensorTag
+
+TODO
+
 ## Setup of SensorTag App
 
-Download the latest SensorTag App from Google Play. Connect to CC2650 Sensortag.
+Download the latest SensorTag App from Google Play or Apple Store. Connect to CC2650 Sensortag. GUI and configuration of SensorTag App may be different on different platform.
 
 ### Cloud View
 
@@ -22,19 +26,11 @@ Turn off Ambient Temperature Data, Barometer Data. Click on Humidity Data, set s
 
 ## Setup of Tago
 
-### Device
-
-Connect your sensor tag with Tago via SensorTag App (refer to [this](https://docs.tago.io/en/articles/9-mqtt-with-sensor-tag)). After adding the device, a bucket with the same name will be created automatically.
-
-Select `Payload Parser` and enable `Run your own parser`. Copy the code in payload_parser.js into the text editor and click save. Different gateway will send different MQTT message, you may need to modify the payload parser.
-
-Copy the default token number of the device.
-
 ### Analysis
 
-Create two analyses, choose Python for `Runtime`, choose External for `Run this script from`. Select `Environment Variables` and add varible `device_token`, the value is what you have copied from last step.
+Create two analyses, choose Python for `Runtime`, choose External for `Run this script from`.
 
-Change the `TOKEN` varible to your `Analysis token` in analysis_rnn.py and analysis_sum.py. Run analysis_rnn.py and analysis_sum.py on your machine.
+Change the `ANALYSIS_TOKEN` varible to your `Analysis token`, change the `ACCOUNT_TOKEN` varible to your `Account token` in analysis_rnn.py and analysis_sum.py. Run analysis_rnn.py and analysis_sum.py on your machine.
 
 Because different gateway will send different MQTT message, names of variables may be fifferent, you may need to modify the code.
 
@@ -45,10 +41,16 @@ pip install -U tago
 
 ### Action
 
-The first action will push data to bucket. Select MQTT Topic for `Type of trigger`, select Insert to Device Bucket for `Type of action`. Select your device and Subscribe to your MQTT Topic.
+The first action will push data to bucket. Select MQTT Topic for `Type of trigger`, select Insert to Device Bucket for `Type of action`. Select your device and Subscribe to your MQTT Topic (which is `data`). Do notice that you need to create this action for every devices.
 
 The second action will call analysis program periodically. Select Schedule for `Type of trigger`, select Run Analysis for `Type of action`. Set interval to 1 minute and select your analysis program.
 
 The third action will call summury program periodically. Select Schedule for `Type of trigger`, select Run Analysis for `Type of action`. Set Trigger to By date and set Recurrence options as your want.
 
-## Collecting Data
+### Register a new device
+
+Create a new device under `Custom MQTT` option. After adding the device, a bucket with the same name will be created automatically.
+
+Select `Payload Parser` and enable `Run your own parser`. Copy the code in payload_parser.js into the text editor and click save. Different gateway will send different MQTT message, you may need to modify the payload parser.
+
+In `Tags` column, add a row to store user's email address. The key is `email`, the value is email address.
--- tago/analysis_rnn.py
+++ tago/analysis_rnn.py
@@ -11,8 +11,9 @@ import torch
 import MODELTEST
 
 # Token of analysis program
-# wyb
-TOKEN = 'b18c2a46-bc9a-41b6-b654-c358bdbf1d0e'
+ANALYSIS_TOKEN = '74b9b103-637b-4ff2-80ff-94c326640122'
+# Token of account
+ACCOUNT_TOKEN = 'b25d1f0c-d3e5-4788-9464-a1d57c922604'
 
 # Load the model from MODELPATH
 if torch.cuda.is_available():
@@ -28,132 +29,146 @@ OUTPUT_SIZE = 3
 checkpoint = torch.load(MODELPATH)
 model = MODELTEST.RNNModel(BATCH_SIZE,HIDDEN_SIZE,OUTPUT_SIZE).to(device)
 model.load_state_dict(checkpoint['model_state_dict'])
-index_to_sport = {0:'walking',1:'running',2:'ropejumping'}
 
+index_to_sport = {0:'walking',1:'running',2:'ropejumping'}
 SAMPLE_NUM = 200
 TOTAL_DATA = 400
 
 def isValid(data):
-    count = 0
-    record = []
-    for dim in range(6):
-        i = 0
-        while(i < 25):
-            flag = False
-            future = [data[j][dim] for j in range(i, i+6)]
-            if(dim < 3):
-                flag = (max(future) - min(future)) < 20
-            else:
-                flag = (max(future) - min(future)) < 0.1
-            if(flag):
-                count += 1
-                record.append((i, dim, future))
-                i += 6
-                if(count == 3):
-                    return False
-            i += 1
-    return True
+  count = 0
+  for dim in range(6):
+    i = 0
+    while(i < 25):
+      flag = False
+      future = [data[j][dim] for j in range(i, i+6)]
+      if(dim < 3):
+        flag = (max(future) - min(future)) < 20
+      else:
+        flag = (max(future) - min(future)) < 0.1
+      if(flag):
+        count += 1
+        i += 6
+        if(count == 3):
+          return False
+      i += 1
+  return True
 
-variables = ['gyrox', 'gyroy', 'gyroz', 'accelx', 'accely', 'accelz']
-#variables = ['gyro_x', 'gyro_y', 'gyro_z', 'acc_x', 'acc_y', 'acc_z']
+# iOS version
+#variables = ['gyrox', 'gyroy', 'gyroz', 'accelx', 'accely', 'accelz']
+# Andriod version
+variables = ['gyro_x', 'gyro_y', 'gyro_z', 'acc_x', 'acc_y', 'acc_z']
 
 def func_callback(context, scope):
-  device_token = context.environment[0]['value']
-  my_device = Device(device_token)
-  
-  filter_gyro_x = {
-    'variable': {variables[0]},
-    'qty' : TOTAL_DATA
-  }
-  result_gyro_x = my_device.find(filter_gyro_x)
-  start_date = result_gyro_x['result'][TOTAL_DATA-1]['time']
-  end_date = result_gyro_x['result'][0]['time']
+  my_account = tago.Account(ACCOUNT_TOKEN)
+  my_devices = my_account.devices.list()
 
-  filter_gyro_y = {
-    'variable': {variables[1]},
-    'start_date' : start_date,
-    'end_date' : end_date,
-    'qty' : TOTAL_DATA
-  }
-  result_gyro_y = my_device.find(filter_gyro_y)
-
-  filter_gyro_z = {
-    'variable': {variables[2]},
-    'start_date' : start_date,
-    'end_date' : end_date,
-    'qty' : TOTAL_DATA
-  }
-  result_gyro_z = my_device.find(filter_gyro_z)
-
-  filter_acc_x = {
-    'variable': {variables[3]},
-    'start_date' : start_date,
-    'end_date' : end_date,
-    'qty' : TOTAL_DATA
-  }
-  result_acc_x = my_device.find(filter_acc_x)
-
-  filter_acc_y = {
-    'variable': {variables[4]},
-    'start_date' : start_date,
-    'end_date' : end_date,
-    'qty' : TOTAL_DATA
-  }
-  result_acc_y = my_device.find(filter_acc_y)
-
-  filter_acc_z = {
-    'variable': {variables[5]},
-    'start_date' : start_date,
-    'end_date' : end_date,
-    'qty' : TOTAL_DATA
-  }
-  result_acc_z = my_device.find(filter_acc_z)
-
-  prediction = {0:0, 1:0, 2:0}
-  random.seed(time.perf_counter())
-  testcases = np.empty(shape=[0, 30, 6])
-  iteration = 0
-  failure = 0
-  
-  for iteration in range(SAMPLE_NUM):
-    input_array = []
-    off = random.randint(0, len(result_gyro_x['result']) - 30)
-    for i in range(off, off+30):
-      input_array.append([float(result_gyro_x['result'][i]['value']),
-                    float(result_gyro_y['result'][i]['value']),
-                    float(result_gyro_z['result'][i]['value']),
-                    float(result_acc_x['result'][i]['value']),
-                    float(result_acc_y['result'][i]['value']),
-                    float(result_acc_z['result'][i]['value'])])
-    # testcases = np.concatenate((testcases, np.array([input_array])))
-    isvalid = isValid(input_array)
-    if(not isvalid):
-      failure += 1
-      continue
+  for i in range(len(my_devices['result'])):
+    device_token = my_account.devices.tokenList(my_devices['result'][i]['id'])
+    my_device = Device(device_token['result'][0]['token'])
     
-    input_array = np.array([input_array])
-    
-    result = MODELTEST.predictSingle(model, torch.from_numpy(input_array), BATCH_SIZE)
-    prediction[result] += 1
-  
-
-  if(failure > SAMPLE_NUM / 2):
-    result = -1
-    print("Doing nothing.")
-  else:
-    prediction = sorted(prediction.items(), key=lambda item:item[1], reverse=True)
-    # print(prediction)
-    result = prediction[0][0]
-    print("Exersise catagory: %s. prob: %.2f" % (index_to_sport[result], prediction[0][1]/(SAMPLE_NUM - failure)))
-
-  insert_data = {
-    'd': {
-      'result': result
+    # fetch data
+    filter_gyro_x = {
+      'variable': {variables[0]},
+      'qty' : TOTAL_DATA
     }
-  }
-  insert_result = my_device.insert(insert_data)
-  print(insert_result)
+    result_gyro_x = my_device.find(filter_gyro_x)
+    start_date = result_gyro_x['result'][len(result_gyro_x['result'])-1]['time']
+    end_date = result_gyro_x['result'][0]['time']
 
+    filter_gyro_y = {
+      'variable': {variables[1]},
+      'start_date' : start_date,
+      'end_date' : end_date,
+      'qty' : TOTAL_DATA
+    }
+    result_gyro_y = my_device.find(filter_gyro_y)
+
+    filter_gyro_z = {
+      'variable': {variables[2]},
+      'start_date' : start_date,
+      'end_date' : end_date,
+      'qty' : TOTAL_DATA
+    }
+    result_gyro_z = my_device.find(filter_gyro_z)
+
+    filter_acc_x = {
+      'variable': {variables[3]},
+      'start_date' : start_date,
+      'end_date' : end_date,
+      'qty' : TOTAL_DATA
+    }
+    result_acc_x = my_device.find(filter_acc_x)
+
+    filter_acc_y = {
+      'variable': {variables[4]},
+      'start_date' : start_date,
+      'end_date' : end_date,
+      'qty' : TOTAL_DATA
+    }
+    result_acc_y = my_device.find(filter_acc_y)
+
+    filter_acc_z = {
+      'variable': {variables[5]},
+      'start_date' : start_date,
+      'end_date' : end_date,
+      'qty' : TOTAL_DATA
+    }
+    result_acc_z = my_device.find(filter_acc_z)
+
+    # do classification
+    if(len(result_gyro_x['result']) >= 30):
+      prediction = {0:0, 1:0, 2:0}
+      random.seed(time.perf_counter())
+      testcases = np.empty(shape=[0, 30, 6])
+      iteration = 0
+      failure = 0
+      insert_flag = 1
+      
+      for iteration in range(SAMPLE_NUM):
+        input_array = []
+        off = random.randint(0, len(result_gyro_x['result']) - 30)
+        for i in range(off, off+30):
+          input_array.append([float(result_gyro_x['result'][i]['value']),
+                              float(result_gyro_y['result'][i]['value']),
+                              float(result_gyro_z['result'][i]['value']),
+                              float(result_acc_x['result'][i]['value']),
+                              float(result_acc_y['result'][i]['value']),
+                              float(result_acc_z['result'][i]['value'])])
+        # testcases = np.concatenate((testcases, np.array([input_array])))
+        isvalid = isValid(input_array)
+        if(not isvalid):
+          failure += 1
+          continue
+        
+        input_array = np.array([input_array])
+        result = MODELTEST.predictSingle(model, torch.from_numpy(input_array), BATCH_SIZE)
+        prediction[result] += 1
+      
+      if(failure > SAMPLE_NUM / 2):
+        print("Doing nothing.")
+        insert_flag = 0
+      else:
+        prediction = sorted(prediction.items(), key=lambda item:item[1], reverse=True)
+        result = prediction[0][0]
+        print("Exersise catagory: %s. prob: %.2f" % (index_to_sport[result], prediction[0][1]/(SAMPLE_NUM - failure)))
+        if(prediction[0][1]/(SAMPLE_NUM - failure) < 0.8):
+          insert_flag = 0
+      
+      if(insert_flag):
+        # Andriod version
+        insert_data = {
+          'd': {
+            'result': result
+          }
+        }
+        # iOS version
+        '''
+        insert_data = {
+          'result': result
+        }
+        '''
+        insert_result = my_device.insert(insert_data)
+        print(insert_result)
 
 if __name__ == '__main__':
-  Analysis(TOKEN).init(func_callback)
+  Analysis(ANALYSIS_TOKEN).init(func_callback)
